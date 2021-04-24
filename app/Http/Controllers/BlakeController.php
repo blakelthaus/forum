b5126d8016d0122754be06803e63b86667193665
Blake Borgholthaus <blake.borgholthaus@aggiemail.usu.edu>
2021-04-23 17:50:49 -0700

    build out cards on homepage with sites and started on skills section

--- app/Http/Controllers/BlakeController.php
+++ app/Http/Controllers/BlakeController.php
@@ -13,7 +13,10 @@ class BlakeController extends Controller
 
     public function new()
     {
-        return view('blake.newindex');
+        $sites = $this->getSites();
+        $colors = $this->getRandomColors();
+        $skills = $this->getSkills();
+        return view('blake.newindex', ['sites' => $sites, 'colors' => $colors, 'skills' => $skills]);
     }
 
     public function contact()
@@ -24,5 +27,157 @@ class BlakeController extends Controller
     public function resume()
     {
         return view('blake.resume');
+    }
+
+    private function getRandomColors() {
+        return [
+            'indigo',
+            'red',
+            'yellow',
+            'green',
+        ];
+    }
+
+    private function getSkills() {
+        return [
+            [
+                'skill' => 'Laravel',
+                'experience' => '2-3 Years',
+                'img' => 'https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/Laravel.svg/1200px-Laravel.svg.png',
+                'link' => 'https://laravel.com/'
+            ],
+            [
+                'skill' => 'SQL (MySQL, Oracle, Sequel)',
+                'experience' => '4 Years',
+                'img' => 'https://pngimg.com/uploads/mysql/mysql_PNG23.png',
+                'link' => 'https://www.mysql.com/'
+            ],
+            [
+                'skill' => 'HTML/CSS',
+                'experience' => '3 Years',
+                'img' => 'https://i.pinimg.com/originals/52/2e/6b/522e6bc1a11d1726a35f81cbd979395f.jpg',
+                'link' => 'https://www.w3schools.com/'
+            ],
+            [
+                'skill' => 'Javascript',
+                'experience' => '3 Years',
+                'img' => 'https://www.vhv.rs/dpng/d/313-3133777_javascript-transparent-background-svg-hd-png-download.png',
+                'link' => 'https://www.w3schools.com/js/js_2018.asp'
+            ],
+            [
+                'skill' => 'jQuery',
+                'experience' => '3 Years',
+                'img' => 'https://lh3.googleusercontent.com/proxy/et4TjXRMYes_x29QuBpE93QpjsLFc_gNCVrwQ5Vs4Aqno0-nE7RB6BfRh2o0rB_ITY6bzLQvTqPLr3Hi51sTJ_h7VTPKUvfZKnedlxW_Qz0e8a3pUXx5vKw',
+                'link' => 'https://jquery.com/'
+            ],
+            [
+                'skill' => 'PHP 5.6-8.0',
+                'experience' => '3 Years',
+                'img' => 'https://www.php.net/images/logos/new-php-logo.svg',
+                'link' => 'https://www.php.net/'
+            ],
+            // [
+            //     'skill' => 'Composer',
+            //     'experience' => '2-3 Years',
+            //     'img' => '',
+            //     'link' => ''
+            // ],
+            // [
+            //     'skill' => 'Apache Servers',
+            //     'experience' => '2 Years',
+            //     'img' => '',
+            //     'link' => ''
+            // ],
+            // [
+            //     'skill' => 'Ngnix Servers',
+            //     'experience' => '1 Year',
+            //     'img' => '',
+            //     'link' => ''
+            // ],
+            // [
+            //     'skill' => 'Angular',
+            //     'experience' => '1 Year',
+            //     'img' => '',
+            //     'link' => ''
+            // ],
+            // [
+            //     'skill' => 'Wordpress',
+            //     'experience' => '2 Years',
+            //     'img' => '',
+            //     'link' => ''
+            // ],
+            // [
+            //     'skill' => '',
+            //     'experience' => '',
+            //     'img' => '',
+            //     'link' => ''
+            // ],
+            // [
+            //     'skill' => '',
+            //     'experience' => '',
+            //     'img' => '',
+            //     'link' => ''
+            // ],
+            // [
+            //     'skill' => '',
+            //     'experience' => '',
+            //     'img' => '',
+            //     'link' => ''
+            // ],
+            
+        ];
+    }
+
+    private function getSites() {
+        return [
+            [
+                'title' => 'VegasGoldenKnights.com',
+                'img' => '/img/VegasGoldenKnightsWebsite.png',
+                'link' => 'https://nhl.com/goldenknights',
+                'description' => 'Work with all departments of the VGK business team to manage the site in multiple ways including: daily content, advertising, scheduling, integrations and relations with league technical teams.'
+            ],
+            [
+                'title' => 'HendersonSilverKnights.com',
+                'img' => '/img/hsk.png',
+                'link' => 'https://hendersonsilverknights.com',
+                'description' => 'Participated in vendor selection and serving as project manager/site admin for the ongoing development of the official website for the newest AHL franchise the Henderson Silver Knights.'
+            ],
+            [
+                'title' => 'VGKWorldwide.com',
+                'img' => '/img/vgkww.png',
+                'link' => 'https://vgkworldwide.com',
+                'description' => 'Manage updates and build new features as necessary for this LAMP stack single page application that allows fans to place their marker on the map via Google Cloud Services and AWS.'
+            ],
+            [
+                'title' => 'VegasGoesGold.com',
+                'img' => '/img/vegasgoesgold.png',
+                'link' => 'https://vegasgoesgold.com',
+                'description' => 'Interactive single-page site built with multiple CSS/JS animation methods to unveil the Vegas Golden Knights new gold jerseys for the 2020-21 season.'
+            ],
+            [
+                'title' => 'VGK Youth Hockey and Rink Sites',
+                'img' => '/img/vgkrinks.png',
+                'link' => 'https://www.vgkrinks.com/',
+                'description' => 'Lead redesign efforts for CityNationalArena.com, VegasGoldenKnightsJr.com, and launched LifeguardArena.com in conjunction with Sportengine.'
+            ],
+            [
+                'title' => 'theDollarLoanCenter.com',
+                'img' => '/img/dlc.png',
+                'link' => 'https://thedollarloancenter.com',
+                'description' => 'Temporary site built to unveil the naming rights of the Dollar Loan Center hosted via AWS S3 and CloudFront. Currently working as project manager on long-term site.'
+            ],
+            [
+                'title' => 'TireGuru.net',
+                'img' => '/img/tireguru.png',
+                'link' => 'https://tireguru.net',
+                'description' => 'Worked with a team of developers to build new features and improve performance of the online TireGuru Business Center Auto and Tire shop management system using Classic PHP, Laravel, and Javascript.'
+            ],
+            [
+                'title' => 'WebTires.net',
+                'img' => '/img/webtires.png',
+                'link' => 'https://webtires.net',
+                'description' => 'Owned codebase for 300+ tire/auto eccomerce and appointment scheduling websites built using a combination of Laravel, Codeigniter, and Angular'
+            ],
+        ];
     }
 }
--- resources/views/blake/newindex.blade.php
+++ resources/views/blake/newindex.blade.php
@@ -24,33 +24,86 @@
     <script src="{{ env('APP_URL') }}/js/tire.js" defer></script>
 </head>
 <body>
-    <nav class="flex items-center justify-between flex-wrap bg-gray-200 p-6 shadow-md dark:bg-gray-700">
-        <div class="flex items-center flex-no-shrink text-white mr-6">
-        <img class="h-12 w-12 mr-2 rounded-full" width="108" height="108" src="/img/blake.jpg" />
-        <span class="font-semibold text-black text-xl tracking-tight dark:text-white">Test Site</span>
+    <div id="main-content" class="bg-gray-100 dark:bg-gray-700">
+        <div class="flex items-center justify-between flex-wrap bg-gray-200 p-6 shadow-lg dark:bg-gray-900">
+            <div class="container">
+                <nav class="flex items-center justify-between flex-wrap">
+                    <div class="flex items-center flex-no-shrink text-white mr-6">
+                    <img class="h-12 w-12 mr-2 rounded-full" width="108" height="108" src="/img/blake.jpg" />
+                    <span class="font-semibold text-black text-xl tracking-tight dark:text-white">Blake Borgholthaus</span>
+                    </div>
+                    <div class="block lg:hidden">
+                    <button class="flex items-center px-3 py-2 border rounded text-black border-black hover:text-white hover:border-white dark:bg-white dark:text-gray-700 dark:border-white" id="hamburger">
+                        <svg class="h-3 w-3" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Menu</title><path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"/></svg>
+                    </button>
+                    </div>
+                    <div class="w-full hidden flex-grow lg:pb-5 lg:flex lg:items-center lg:w-auto" id="mobile-nav">
+                    <div class="text-sm lg:flex-grow">
+                        <a href="" class="block mt-4 lg:inline-block lg:mt-0 text-black hover:text-gray-600 mr-4 lg:ml-4 dark:text-white">
+                        Sites
+                        </a>
+                        <a href="" class="block mt-4 lg:inline-block lg:mt-0 text-black hover:text-gray-600 mr-4 dark:text-white">
+                        Skills
+                        </a>
+                        <a href="" class="block mt-4 lg:inline-block lg:mt-0 text-black hover:text-gray-600 dark:text-white">
+                        Social
+                        </a>
+                    </div>
+                    <div>
+                        <a href="#" id="color-mode" class=" inline-block text-sm px-4 py-2 shadow-md leading-none border rounded text-black border-gray-900 transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-105 hover:border-transparent hover:text-white hover:bg-black mt-4 lg:mt-0 dark:text-white dark:border-white dark:hover:bg-white dark:hover:text-black">Dark Mode</a>
+                    </div>
+                    </div>
+                </nav>
+            </div>
         </div>
-        <div class="block lg:hidden">
-        <button class="flex items-center px-3 py-2 border rounded text-black border-black hover:text-white hover:border-white dark:bg-white dark:text-gray-700 dark:border-white" id="hamburger">
-            <svg class="h-3 w-3" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Menu</title><path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"/></svg>
-        </button>
+        {{-- body content to be split from header once done prototyping --}}
+        <div class="container my-12 mx-auto px-4 md:px-12" id="sites">
+            <h2 class="text-4xl dark:text-white">Websites</h2>
+            <div class="flex flex-wrap -mx-1 lg:-mx-4">
+            @foreach($sites as $key => $site)
+                    <!-- Column -->
+                    <div class="my-1 px-1 w-full md:w-1/2 lg:my-4 lg:px-4 lg:w-1/4">
+                        <!-- Article -->
+                        <article class="transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-105 overflow-hidden rounded-lg shadow-lg dark:bg-gray-900 ">
+                            <a href="{{ $site['link'] }}">
+                                <img alt="{{ $site['title'] }}" class="block h-auto w-full" src="{{ $site['img'] }}">
+                            </a>
+                            <header class="flex items-center justify-between leading-tight p-2 md:p-4">
+                                <h1 class="text-lg">
+                                    <a class="no-underline hover:underline text-black dark:text-white dark:hover:text-gray-300" href="https://vegasgoesgold.com">
+                                        {{ $site['title'] }}
+                                    </a>
+                                </h1>
+                            </header>
+                            <footer class="flex items-center justify-between leading-none p-2 md:p-4">
+                                <a class="flex items-center no-underline text-black ml-2 text-sm hover:text-black hover:no-underline dark:text-white dark:hover:text-gray-300" href="#">    
+                                    {{ $site['description'] }}
+                                </a>
+                            </footer>
+                        </article>
+                        <!-- END Article -->
+                    </div>
+            @endforeach
+            </div>
         </div>
-        <div class="w-full hidden flex-grow lg:pb-5 lg:flex lg:items-center lg:w-auto" id="mobile-nav">
-        <div class="text-sm lg:flex-grow">
-            <a href="#responsive-header" class="block mt-4 lg:inline-block lg:mt-0 text-black hover:text-gray-600 mr-4 lg:ml-4 dark:text-white">
-            Docs
-            </a>
-            <a href="#responsive-header" class="block mt-4 lg:inline-block lg:mt-0 text-black hover:text-gray-600 mr-4 dark:text-white">
-            Examples
-            </a>
-            <a href="#responsive-header" class="block mt-4 lg:inline-block lg:mt-0 text-black hover:text-gray-600 dark:text-white">
-            Blog
-            </a>
+        <div class="container my-12 mx-auto px-4 md:px-12" id="skills">
+            <h2 class="text-4xl dark:text-white">Skills</h2>
+            <div class="flex flex-wrap -mx-1 lg:-mx-4 p-4">
+                @foreach($skills as $key => $skill)
+                    <div class="shadow-sm dark:bg-gray-800 bg-opacity-95 border-opacity-60 w-full md:w-1/2 lg:my-4 lg:px-4 lg:w-1/4 border-solid rounded-3xl border-2 | flex justify-around cursor-pointer | hover:bg-{{ $colors[array_rand($colors)] }}-400 dark:hover:bg-{{ $colors[array_rand($colors)] }}-600 hover:border-transparent | transition-colors duration-500">
+                        <img class="max-h-16 object-cover" src="{{ $skill['img'] }}" alt="{{ $skill['skill'] }}" />
+                        <div class="flex flex-col justify-center">
+                            <p class="text-gray-900 dark:text-gray-300 font-semibold">{{ $skill['skill'] }}</p>
+                            <p class="text-black dark:text-gray-100 text-justify">{{ $skill['experience'] }}</p>
+                        </div>
+                    </div>
+                @endforeach
+            </div>
         </div>
-        <div>
-            <a href="#" id="color-mode" class="inline-block text-sm px-4 py-2 shadow-md leading-none border rounded text-black border-gray-900 hover:border-transparent hover:text-white hover:bg-black mt-4 lg:mt-0 dark:text-white dark:border-white dark:hover:bg-white dark:hover:text-black">Dark Mode</a>
+        <div class="" id="resume">
+
         </div>
-        </div>
-    </nav>
+    </div>
 
     <style>
         .active{
@@ -58,7 +111,7 @@
         }    
     </style>
 
-    {{-- Script to expand the hamburger menu on mobile --}}
+    {{-- expand and hide the hamburger menu on mobile --}}
     <script>
         let hamburger = document.getElementById('hamburger');
     
@@ -69,6 +122,7 @@
         });
     </script>
 
+    {{-- Toggle dark/light modes --}}
     <script>
         let colorButton = document.getElementById('color-mode');
 
@@ -83,8 +137,6 @@
                 colorButton.textContent = 'Light Mode';
             }
         });
-
     </script>
-      
 </body>
 
