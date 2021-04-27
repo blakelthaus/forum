79883e3ee206c9d73c0dc70f9f360e8df4faae65
Blake Borgholthaus <blake.borgholthaus@aggiemail.usu.edu>
2021-04-27 16:50:46 -0700

    adding functionality to new homepage

--- app/Http/Controllers/BlakeController.php
+++ app/Http/Controllers/BlakeController.php
@@ -41,83 +41,131 @@ class BlakeController extends Controller
     private function getSkills() {
         return [
             [
+                'skill' => 'SQL (MySQL, Oracle, Sequel)',
+                'experience' => '4 Years',
+                'img' => 'https://brandslogos.com/wp-content/uploads/images/large/mysql-logo-1.png',
+                'link' => 'https://www.mysql.com/'
+            ],
+            [
+                'skill' => 'PHP 5.6-8.0',
+                'experience' => '3 Years',
+                'img' => 'https://cdn.iconscout.com/icon/free/png-512/php-2038871-1720084.png',
+                'link' => 'https://www.php.net/'
+            ],
+            [
                 'skill' => 'Laravel',
-                'experience' => '2-3 Years',
+                'experience' => '3 Years',
                 'img' => 'https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/Laravel.svg/1200px-Laravel.svg.png',
                 'link' => 'https://laravel.com/'
             ],
             [
-                'skill' => 'SQL (MySQL, Oracle, Sequel)',
-                'experience' => '4 Years',
-                'img' => 'https://pngimg.com/uploads/mysql/mysql_PNG23.png',
-                'link' => 'https://www.mysql.com/'
+                'skill' => 'HTML',
+                'experience' => '3 Years',
+                'img' => 'https://images.vexels.com/media/users/3/166383/isolated/preview/6024bc5746d7436c727825dc4fc23c22-html-programming-language-icon-by-vexels.png',
+                'link' => 'https://www.w3schools.com/'
             ],
             [
-                'skill' => 'HTML/CSS',
+                'skill' => 'CSS',
                 'experience' => '3 Years',
-                'img' => 'https://i.pinimg.com/originals/52/2e/6b/522e6bc1a11d1726a35f81cbd979395f.jpg',
+                'img' => 'https://cdn.iconscout.com/icon/free/png-512/css3-8-1175200.png',
                 'link' => 'https://www.w3schools.com/'
             ],
             [
                 'skill' => 'Javascript',
                 'experience' => '3 Years',
-                'img' => 'https://www.vhv.rs/dpng/d/313-3133777_javascript-transparent-background-svg-hd-png-download.png',
+                'img' => 'https://cdn.iconscout.com/icon/free/png-256/javascript-2038874-1720087.png',
                 'link' => 'https://www.w3schools.com/js/js_2018.asp'
             ],
             [
                 'skill' => 'jQuery',
                 'experience' => '3 Years',
-                'img' => 'https://lh3.googleusercontent.com/proxy/et4TjXRMYes_x29QuBpE93QpjsLFc_gNCVrwQ5Vs4Aqno0-nE7RB6BfRh2o0rB_ITY6bzLQvTqPLr3Hi51sTJ_h7VTPKUvfZKnedlxW_Qz0e8a3pUXx5vKw',
+                'img' => 'https://cdn.iconscout.com/icon/free/png-512/jquery-10-1175155.png',
                 'link' => 'https://jquery.com/'
             ],
             [
-                'skill' => 'PHP 5.6-8.0',
+                'skill' => 'Composer',
                 'experience' => '3 Years',
-                'img' => 'https://www.php.net/images/logos/new-php-logo.svg',
-                'link' => 'https://www.php.net/'
+                'img' => 'https://camo.githubusercontent.com/fae4c4781b893f7229d68c9047a805e9a1dced12e13b466fe40ffd89f0eb6e52/687474703a2f2f676574636f6d706f7365722e6f72672f696d672f6c6f676f2d636f6d706f7365722d7472616e73706172656e742e706e67',
+                'link' => 'https://getcomposer.org/'
             ],
-            // [
-            //     'skill' => 'Composer',
-            //     'experience' => '2-3 Years',
-            //     'img' => '',
-            //     'link' => ''
-            // ],
-            // [
-            //     'skill' => 'Apache Servers',
-            //     'experience' => '2 Years',
-            //     'img' => '',
-            //     'link' => ''
-            // ],
-            // [
-            //     'skill' => 'Ngnix Servers',
-            //     'experience' => '1 Year',
-            //     'img' => '',
-            //     'link' => ''
-            // ],
-            // [
-            //     'skill' => 'Angular',
-            //     'experience' => '1 Year',
-            //     'img' => '',
-            //     'link' => ''
-            // ],
-            // [
-            //     'skill' => 'Wordpress',
-            //     'experience' => '2 Years',
-            //     'img' => '',
-            //     'link' => ''
-            // ],
-            // [
-            //     'skill' => '',
-            //     'experience' => '',
-            //     'img' => '',
-            //     'link' => ''
-            // ],
-            // [
-            //     'skill' => '',
-            //     'experience' => '',
-            //     'img' => '',
-            //     'link' => ''
-            // ],
+            [
+                'skill' => 'AWS',
+                'experience' => '3 Years',
+                'img' => 'https://img.icons8.com/color/452/amazon-web-services.png',
+                'link' => 'https://aws.amazon.com/'
+            ],
+            [
+                'skill' => 'Github',
+                'experience' => '3 Years',
+                'img' => 'https://image.flaticon.com/icons/png/512/25/25231.png',
+                'link' => 'https://github.com/blakelthaus'
+            ],
+            [
+                'skill' => 'Apache Servers',
+                'experience' => '2 Years',
+                'img' => 'https://d22e4d61ky6061.cloudfront.net/sites/default/files/Apache%20HHTP%20Server.png',
+                'link' => 'https://httpd.apache.org/'
+            ],
+            [
+                'skill' => 'Ngnix Servers',
+                'experience' => '2 Year',
+                'img' => 'https://img.icons8.com/color/452/nginx.png',
+                'link' => 'https://www.nginx.com/'
+            ],
+            [
+                'skill' => 'Angular',
+                'experience' => '2 Years',
+                'img' => 'https://angular.io/assets/images/logos/angular/angular.png',
+                'link' => 'https://angular.io/'
+            ],
+            [
+                'skill' => 'Wordpress',
+                'experience' => '2 Years',
+                'img' => 'https://cdn.iconscout.com/icon/free/png-512/wordpress-35-569289.png',
+                'link' => 'https://wordpress.com/'
+            ],
+            [
+                'skill' => 'Code Igniter',
+                'experience' => '2 Years',
+                'img' => 'https://cdn.iconscout.com/icon/free/png-512/codeigniter-4-1175201.png',
+                'link' => 'https://codeigniter.com/'
+            ],
+            [
+                'skill' => 'Bitbucket',
+                'experience' => '2 Years',
+                'img' => 'https://cdn4.iconfinder.com/data/icons/logos-and-brands/512/44_Bitbucket_logo_logos-512.png',
+                'link' => 'https://bitbucket.org/'
+            ],
+            [
+                'skill' => 'Beanstalk',
+                'experience' => '2 Years',
+                'img' => 'https://numediaweb.com/wordpress/wp-content/uploads/2014/05/beanstalk-logo-trans.png',
+                'link' => ''
+            ],
+            [
+                'skill' => 'Vagrant VM',
+                'experience' => '2 Years',
+                'img' => 'https://cdn.iconscout.com/icon/free/png-512/vagrant-226063.png',
+                'link' => 'https://beanstalkapp.com/'
+            ],
+            [
+                'skill' => 'Homebrew',
+                'experience' => '2 Years',
+                'img' => 'https://icons-for-free.com/iconfiles/png/512/homebrew-1324440174216266294.png',
+                'link' => 'https://brew.sh/'
+            ],
+            [
+                'skill' => 'Python',
+                'experience' => '1 Year',
+                'img' => 'https://cdn3.iconfinder.com/data/icons/logos-and-brands-adobe/512/267_Python-512.png',
+                'link' => 'https://www.python.org/'
+            ],
+            [
+                'skill' => 'Squarespace Developer Platform',
+                'experience' => '1 year',
+                'img' => 'https://cdn.iconscout.com/icon/free/png-512/squarespace-3-739563.png',
+                'link' => 'https://support.squarespace.com/hc/en-us'
+            ],
             // [
             //     'skill' => '',
             //     'experience' => '',
@@ -176,7 +224,7 @@ class BlakeController extends Controller
                 'title' => 'WebTires.net',
                 'img' => '/img/webtires.png',
                 'link' => 'https://webtires.net',
-                'description' => 'Owned codebase for 300+ tire/auto eccomerce and appointment scheduling websites built using a combination of Laravel, Codeigniter, and Angular'
+                'description' => 'Owned codebase for 300+ tire/auto eccomerce and appointment scheduling websites built using a combination of Laravel, Codeigniter, and Angular. Webtires.net is one example of many.'
             ],
         ];
     }
--- resources/views/blake/newindex.blade.php
+++ resources/views/blake/newindex.blade.php
@@ -90,11 +90,13 @@
             <h2 class="text-4xl dark:text-white">Skills</h2>
             <div class="flex flex-wrap -mx-1 lg:-mx-4 p-4">
                 @foreach($skills as $key => $skill)
-                    <div class="shadow-sm dark:bg-gray-800 bg-opacity-95 border-opacity-60 w-full md:w-1/2 lg:my-4 lg:px-4 lg:w-1/4 border-solid rounded-3xl border-2 | flex justify-around cursor-pointer | hover:bg-{{ $colors[array_rand($colors)] }}-400 dark:hover:bg-{{ $colors[array_rand($colors)] }}-600 hover:border-transparent | transition-colors duration-500">
-                        <img class="max-h-16 object-cover" src="{{ $skill['img'] }}" alt="{{ $skill['skill'] }}" />
-                        <div class="flex flex-col justify-center">
-                            <p class="text-gray-900 dark:text-gray-300 font-semibold">{{ $skill['skill'] }}</p>
-                            <p class="text-black dark:text-gray-100 text-justify">{{ $skill['experience'] }}</p>
+                    <div class="my-1 px-1 w-full md:w-1/3 lg:my-4 lg:px-4 lg:w-1/5">
+                        <div class="shadow-sm dark:bg-gray-800 bg-opacity-95 border-opacity-60 border-solid rounded-3xl border-2 | flex justify-around cursor-pointer | hover:bg-{{ $colors[array_rand($colors)] }}-400 dark:hover:bg-{{ $colors[array_rand($colors)] }}-600 hover:border-transparent | transition-colors duration-500">
+                            <img class="w-16 h-16 object-cover p-2" src="{{ $skill['img'] }}" alt="{{ $skill['skill'] }}" />
+                            <div class="flex flex-col justify-center">
+                                <p class="text-gray-900 dark:text-gray-300 font-semibold">{{ $skill['skill'] }}</p>
+                                <p class="text-black dark:text-gray-100 text-justify">{{ $skill['experience'] }}</p>
+                            </div>
                         </div>
                     </div>
                 @endforeach
